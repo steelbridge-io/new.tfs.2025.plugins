3ef3ddd58933bc3e0d39d1a690f928a0677633bd
steelbridge-io <chris@steelbridge.io>
2025-04-29 21:19:03 -0700

    Extend custom fields to additional page template and post type
    Added support for the 'regional-waters-template.php' page template and the 'guide_service' custom post type. This ensures relevant meta boxes are displayed for newly included content types.

--- tfs-custom-fields/sbm_custom_fields_travel.php
+++ tfs-custom-fields/sbm_custom_fields_travel.php
@@ -16,8 +16,8 @@ function tfs_custom_travel_meta() {
     global $post;
     if(!empty($post)){
         $pageTemplate = get_post_meta($post->ID, '_wp_page_template', true);
-        if($pageTemplate == 'page-templates/destination-template.php') {
-            $types = array('post', 'page', 'travel_cpt', 'lower48', 'travel-blog', 'esb_lodge');
+        if($pageTemplate == 'page-templates/destination-template.php' || $pageTemplate == 'page-templates/regional-waters-template.php') {
+            $types = array('post', 'page', 'travel_cpt', 'lower48', 'travel-blog', 'esb_lodge', 'guide_service');
             foreach($types as $type) {
                 add_meta_box( 'sbm_meta', __( 'Travel Content Fields', 'tfs-travel-textdomain' ), 'tfs_travel_meta_callback',
                     $type, 'normal', 'high' );
