763aa3aed5c9fc7a8b3cc3c11868fa24b6571908
steelbridge-io <chris@steelbridge.io>
2026-02-17 23:36:11 -0800

    Enable responsive hero meta box for staff-template.php

--- meta-field-content-plugin/includes/responsive-hero-meta.php
+++ meta-field-content-plugin/includes/responsive-hero-meta.php
@@ -31,6 +31,7 @@ function tfs_responsive_hero_meta_box() {
             'page-templates/signature-template.php',
             'page-templates/guide-service-template.php',
             'page-templates/guide-destination-template.php',
+            'page-templates/staff-template.php',
             // Add more templates here as we roll this out:
             // 'page-templates/private-waters-template-v3.php',
             // 'page-templates/schools-template-v3.php',
