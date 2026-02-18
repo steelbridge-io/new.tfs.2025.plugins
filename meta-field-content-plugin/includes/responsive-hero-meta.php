43be45ceba12e5760587240cace5f31aaea3bfd3
steelbridge-io <chris@steelbridge.io>
2026-02-17 23:45:03 -0800

    Enable responsive hero meta box for travel-lodges-camps-template.php

--- meta-field-content-plugin/includes/responsive-hero-meta.php
+++ meta-field-content-plugin/includes/responsive-hero-meta.php
@@ -32,6 +32,7 @@ function tfs_responsive_hero_meta_box() {
             'page-templates/guide-service-template.php',
             'page-templates/guide-destination-template.php',
             'page-templates/staff-template.php',
+            'page-templates/travel-lodges-camps-template.php',
             // Add more templates here as we roll this out:
             // 'page-templates/private-waters-template-v3.php',
             // 'page-templates/schools-template-v3.php',
