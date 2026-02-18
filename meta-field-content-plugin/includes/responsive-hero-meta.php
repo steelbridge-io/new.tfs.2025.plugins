5c7189fd620f052d687488431e46a7e7e721d99d
steelbridge-io <chris@steelbridge.io>
2026-02-18 00:20:39 -0800

    Enable responsive hero meta box for fly-fishing-schools-template.php

--- meta-field-content-plugin/includes/responsive-hero-meta.php
+++ meta-field-content-plugin/includes/responsive-hero-meta.php
@@ -36,6 +36,7 @@ function tfs_responsive_hero_meta_box() {
             'page-templates/sections-template.php',
             'page-templates/private-waters-template.php',
             'page-templates/private-waters-template-v3.php',
+            'page-templates/fly-fishing-schools-template.php',
             // Add more templates here as we roll this out:
             // 'page-templates/private-waters-template-v3.php',
             // 'page-templates/schools-template-v3.php',
