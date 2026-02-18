8fd4d3820f928d8ad29bd94c695f22b83a5f85a2
steelbridge-io <chris@steelbridge.io>
2026-02-18 00:47:05 -0800

    Enable responsive hero meta box for news-blog-wide-template.php

--- meta-field-content-plugin/includes/responsive-hero-meta.php
+++ meta-field-content-plugin/includes/responsive-hero-meta.php
@@ -41,6 +41,7 @@ function tfs_responsive_hero_meta_box() {
             'page-templates/fish-camp-template.php',
             'page-templates/basic-page-template.php',
             'page-templates/stream-report-template.php',
+            'page-templates/news-blog-wide-template.php',
             // Add more templates here as we roll this out:
             // 'page-templates/private-waters-template-v3.php',
             // 'page-templates/schools-template-v3.php',
